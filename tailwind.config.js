5e5198925ae8fb1f8f3c3a36e7578727b4f18032
Breno Nogueira Araujo <breno.nog.araujo@hotmail.com>
2021-12-07 08:29:42 -0400

    TESTE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/*.{js,ts,jsx,tsx}'],
   darkMode: media, // or 'media' or 'class'
   theme: {
     extend: {},
