6735b5a4552033f86cf9ea7edb858037c9f6c2e8
Breno Nogueira Araujo <breno.nog.araujo@hotmail.com>
2021-12-07 08:19:45 -0400

    srsdsds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './src/components/*.{jsx, js}', './src/layout/*.{js,jsx}'],
+  purge: ['./pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}', './src/components/*.{jsx, js}', './layout/*.{js,jsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
